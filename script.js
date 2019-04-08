f0431e8af16fe478cf7b1e0bd785b6d01a359e4f
terrencethepro <43037019+terrencethepro@users.noreply.github.com>
2019-04-08 08:59:12 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,12 @@
+$(".cedric").click(function() {
+    $(".sofia").slideToggle();
+});
+
+$(".cedric1").click(function() {
+    $(".clover").fadeToggle();
+});
+
+$(".cedric2").click(function() {
+    $(".bird").toggle();
+});
+
